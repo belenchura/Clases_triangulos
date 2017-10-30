9227f2757739617a0f70736b5e7f12dc6b138238
Belen Chura <belenchura@gmail.com>
2017-10-30 15:54:24 -0300

    Subo primera version. Prueba de clases

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clases_triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
